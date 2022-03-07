8af27e73e9386d54a1444b0b058a1e49b55bc08d
Térence Chateigné <terencec@padok.fr>
2022-03-07 18:19:33 +0100

    fix(managed-node-groups): use prefix separator for launch templates name prefixes

--- node_groups.tf
+++ node_groups.tf
@@ -323,7 +323,8 @@ module "eks_managed_node_group" {
   security_group_rules           = try(each.value.security_group_rules, var.eks_managed_node_group_defaults.security_group_rules, {})
   security_group_tags            = try(each.value.security_group_tags, var.eks_managed_node_group_defaults.security_group_tags, {})
 
-  tags = merge(var.tags, try(each.value.tags, var.eks_managed_node_group_defaults.tags, {}))
+  tags             = merge(var.tags, try(each.value.tags, var.eks_managed_node_group_defaults.tags, {}))
+  prefix_separator = var.prefix_separator
 }
 
 ################################################################################
@@ -450,5 +451,6 @@ module "self_managed_node_group" {
   security_group_rules           = try(each.value.security_group_rules, var.self_managed_node_group_defaults.security_group_rules, {})
   security_group_tags            = try(each.value.security_group_tags, var.self_managed_node_group_defaults.security_group_tags, {})
 
-  tags = merge(var.tags, try(each.value.tags, var.self_managed_node_group_defaults.tags, {}))
+  tags             = merge(var.tags, try(each.value.tags, var.self_managed_node_group_defaults.tags, {}))
+  prefix_separator = var.prefix_separator
 }
